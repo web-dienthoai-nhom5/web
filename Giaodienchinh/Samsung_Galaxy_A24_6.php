4cadcf4df6562190febda980866bb4cba829496c
kaitluv <100707304+Kimzies@users.noreply.github.com>
2023-12-25 10:44:53 +0700

    Update Samsung_Galaxy_A24_6.php

--- Giaodienchinh/Samsung_Galaxy_A24_6.php
+++ Giaodienchinh/Samsung_Galaxy_A24_6.php
@@ -21,7 +21,7 @@ session_start();
         </div>
         <table border="0" cellpadding="2px"width="600px">
             <?php
-                $result = mysqli_query($conn, "SELECT * FROM sanpham WHERE MASP=15") or die ("SELECT * FROM sanpham WHERE MASP=15" . "<br/><br/>" . mysqli_error($conn));
+                $result = mysqli_query($conn, "SELECT * FROM sanpham WHERE MASP=5") or die ("SELECT * FROM sanpham WHERE MASP=5" . "<br/><br/>" . mysqli_error($conn));
                 while($row=mysqli_fetch_assoc($result)){
                     ?>
                     <tr>
