133bd708fdb5fd0aed5adadefbac7067e9137a7f
Annage123 <100896220+Annage123@users.noreply.github.com>
2023-12-25 18:09:03 +0700

    Update xoasanpham.php

--- Giaodienchinh/xoasanpham.php
+++ Giaodienchinh/xoasanpham.php
@@ -1,23 +1,26 @@
 <?php
 session_start();
 
-if (isset($_POST['Masp'])) {
-    $masp = $_POST['Masp'];
-
-    // Kiểm tra giỏ hàng đã tồn tại chưa
-    if (isset($_SESSION['giohang'])) {
-        // Kiểm tra sản phẩm có trong giỏ hàng không
-        foreach ($_SESSION['giohang'] as $key => $item) {
-            if ($item['Masp'] == $masp) {
-                // Xóa sản phẩm
-                unset($_SESSION['giohang'][$key]);
-                break;
-            }
-        }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Kiểm tra xem masp có được thiết lập trong yêu cầu hay không
+    if(isset($_POST['masp'])) {
+        $product_id = $_POST['masp'];
+    
+        // Debug: In ra giá trị mã sản phẩm
+        var_dump($product_id);
+    
+        // Xóa sản phẩm khỏi session giỏ hàng
+        unset($_SESSION['giohang'][$product_id]);
+        
+        // Chuyển hướng người dùng trở lại trang giohang.php
+        header("location: giohang.php");
+        exit();
+    } else {
+        echo "Mã sản phẩm không được thiết lập trong yêu cầu. Không thể xóa sản phẩm.";
     }
+} else {
+    // Đối với truy cập trực tiếp vào trang xoasanpham.php
+    header("location: giohang.php");
+    exit();
 }
-
-// Chuyển hướng về trang giohang.php
-header("Location: giohang.php");
-exit();
 ?>
