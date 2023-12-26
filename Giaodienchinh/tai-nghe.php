f80d1e7a7bab8c526c1befc34bf0f400a84b65dd
kaitluv <100707304+Kimzies@users.noreply.github.com>
2023-12-26 10:36:37 +0700

    Update tai-nghe.php

--- Giaodienchinh/tai-nghe.php
+++ Giaodienchinh/tai-nghe.php
@@ -24,6 +24,30 @@
                     </div>
                     </a>
                 </td>
+                <td>
+                        <div class="warpper-group-category" data-index='2'>
+                        <a href="tai-nghe3.php">
+                            <img src="https://cdn.tgdd.vn/Products/Images/54/201517/tai-nghe-ep-mozard-ds510-wb-xanh-up-1-org.jpg" class='img'    style="width:50%; border-radius: 50px;" >
+                            <br>
+                            <br>
+                            <a>Tai nghe Có Dây Mozard DS510-WB</a>
+                            <br>
+                            <div class="discount-price"> 75.000đ</div>
+                        </a>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="warpper-group-category" data-index='2'>
+                        <a href="tai-nghe4.php">
+                            <img src="https://cdn.tgdd.vn/Products/Images/54/248455/samsung-galaxy-buds-2-r177n-trang-1.jpg" class='img'    style="width:50%; border-radius: 50px;" >
+                            <br>
+                            <br>
+                            <a>Tai nghe Bluetooth True Wireless Samsung Galaxy Buds 2 R177</a>
+                            <br>
+                            <div class="discount-price"> 2.990.000đ</div>
+                        </a>
+                        </div>
+                    </td>
             </tr>
             <tr></tr>
         </table>
